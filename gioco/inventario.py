961b5783fe46156eba2ddf85cbd0ec35358499e9
KonradC1 <konrad05@gmail.com>
2025-06-16 13:12:47 +0200

    completamento funzioni principali di inventario

--- gioco/inventario.py
+++ gioco/inventario.py
@@ -1,11 +1,11 @@
 from .oggetto import Oggetto
 from .personaggio import Personaggio
 from .ambiente import Ambiente
-from utils.log import Log
-from utils.salvataggio import SerializableMixin, Json
-@SerializableMixin.register_class
+# from utils.log import Log
+# from utils.salvataggio import SerializableMixin, Json
+# @SerializableMixin.register_class
 
-class Inventario(SerializableMixin):
+class Inventario():
     """
     Gestisce la lista di oggetti posseduto da ogni personaggio
     Sarà la classe inventario a gestire le istanze di classe Oggetto
@@ -27,14 +27,50 @@ class Inventario(SerializableMixin):
         """
         self.oggetti.append(oggetto)
         msg = f"Aggiunto l'oggetto '{oggetto.nome}' all inventario. "
-        return
+        return msg
 
-    def cerca_oggetto(self, oggetto: Oggetto)->str:
-        pass
-
-    def mostra_inventario(self)-> tuple[list[Oggetto], str]:
+    def _aggiungi(self, oggetto: Oggetto)-> None:
         """
-        con una tupla invia una copia del
+        Aggiunge un oggetto all'inventario.
+
+        Args:
+            oggetto (Oggetto): L'oggetto da aggiungere all'inventario.
+
+        Return:
+            None
+        """
+        self.oggetti.append(oggetto)
+
+    def cerca_oggetto(self, oggetto: Oggetto)-> bool | str:
+        """
+        cerca un oggetto specifico nell'inventario
+        ritorna true se è presente o false se non c'è
+
+        Args:
+            oggetto (Oggetto): l'elemento da cercare all'interno della lista interna oggetti
+
+        Returns:
+            found (bool): risultato previsto della funzione per cercare un oggetto specifico
+                ritorna true se viene trovato
+                ritorna false se non è presente
+
+            msg (str): Messaggio di errore.
+        """
+        msg =""
+        try:
+            found = False
+            for obj in self.oggetti:
+                if obj is oggetto:
+                    found = True
+                    break
+            return found
+        except Exception as e:
+            msg = f"Errore generico: {e}"
+            return msg
+
+    def mostra_inventario(self)->str:
+        """
+        invia una stringa con la lista dei nomi degli oggetti presenti
 
         Args:
             None
@@ -52,166 +88,73 @@ class Inventario(SerializableMixin):
                 msg +=f"-{oggetto.nome}\n"
         return msg
 
-    def usa_oggetto(self, oggetto : Oggetto, bersaglio= None, ambiente: Ambiente = None)->tuple[str, int]:
-        pass
-
-    def riversa_inventario(self):
-        pass
-
-    
-    # def usa_oggetto(self, oggetto : Oggetto, bersaglio= None, ambiente: Ambiente = None)->None:
-    #     """
-    #     Utilizza un oggetto presente nell'inventario.
-
-    #     Args:
-    #         oggetto (Oggetto): oggetto da usare.
-    #         utilizzatore (Personaggio): Il Personaggio che usa l'oggetto.
-    #         bersaglio(Any): None di Default è un parametro opzionale che
-    #         permette di usare un oggetto su un altro Personaggio che non sia l'utilizzatore.
-
-    #     Return:
-    #         None
-
-    #     """
-    #     msg = ""
-    #     if self.proprietario :
-    #         utilizzatore = self.proprietario
-    #         for obj in self.oggetti :
-    #             if obj is oggetto :
-    #                 if ambiente is None:
-    #                     mod_ambiente = 0
-    #                 else:
-    #                      mod_ambiente = ambiente.modifica_effetto_oggetto(oggetto)
-    #                 msg = oggetto.usa(utilizzatore, bersaglio, mod_ambiente=mod_ambiente)
-    #                 self.oggetti.remove(oggetto)
-    #                 return
-    #         msg = f"{utilizzatore.nome} non ha {oggetto.nome}."
-    #         Log.scrivi_log(msg)
-    #     else:
-    #         msg = "Per usare un oggetto, l'inventario deve avere un proprietario"
-
-    # def cerca_e_usa(self, bersaglio=None, ambiente : Ambiente = None )-> None:
-    #     """
-    #     Mostra gli oggetti contenuti nell'inventario e permette di scegliere
-    #     l'oggetto da usare attraverso l'indice.
-    #     Chiama il metodo usa_oggetto
-
-    #     Args:
-    #         bersaglio(Any): Parametro opzionale , bersaglio su cui utilizzare
-    #         l'oggetto se offensivo.
-    #         ambient(Ambiente): L'ambiente può alterare il funzionamento degli
-    #         oggetti.
-
-    #     Return:
-    #         None
-    #     """
-    #     if len(self.oggetti)!= 0:
-    #         print("Inventario :")
-    #         for indx, obj in enumerate(self.oggetti, start=1):
-    #             print(f"{indx}- {obj.nome}")
-    #         idx_obj_scelto = IU.chiedi_numero("Quale oggetto vuoi usare ? ",minimo= 1,massimo= len(self.oggetti))
-    #         self.usa_oggetto(self.oggetti[idx_obj_scelto], bersaglio=bersaglio, ambiente=ambiente )
-    #     else:
-    #         testo= "L'inventario è vuoto"
-    #         print(testo)
-    #         Log.scrivi_log(testo)
-
-    # def cerca_e_usa_multi(
-    #         self,
-    #         bersagli: list[Personaggio] = None,
-    #         ambiente: Ambiente = None
-    # ) -> None:
-
-    #     """
-    #     Mostra gli oggetti contenuti nell'inventario e permette di scegliere
-    #     l'oggetto da usare attraverso l'indice.
-    #     Chiama il metodo usa_oggetto
-
-    #     Args:
-    #         bersagli (list(Any)): Parametro opzionale , bersaglio su cui
-    #         utilizzare l'oggetto
-    #         ambiente (Ambiente): L'ambiente può alterare il funzionamento degli
-    #         oggetti
-
-    #     Return:
-    #         None
-    #     """
-    #     if len(self.oggetti) != 0:
-    #         print("Inventario :")
-    #         for indx, obj in enumerate(self.oggetti, start=1):
-    #             print(f"{indx}- {obj.nome}")
-    #         print(f"{len(self.oggetti) + 1}- esci dall'inventario")
-    #         n_scelte_obj = len(self.oggetti)+1
-    #         idx_obj_scelto = -1 + IU.chiedi_numero(
-    #             "Quale oggetto vuoi usare ? ",
-    #             minimo=1,
-    #             massimo= n_scelte_obj
-    #         )
-    #         if idx_obj_scelto+1 == n_scelte_obj :  # se scelgo nessuno
-    #             print("Nessun oggetto scelto. si esce dall'inventario")
-    #             return
-
-    #         # situazioni particolari
-    #         if bersagli is None:
-    #             bersagli = []
-
-    #         # se anziché una lista di bersagli è un Personaggio singolo
-    #         if isinstance(bersagli, Personaggio):
-    #             bersagli = [bersagli]
-    #         if self.proprietario not in bersagli:
-    #             # Aggiungo il proprietario come bersaglio disponibile
-    #             # se non è già presente nella lista dei bersagli
-    #             bersagli.append(self.proprietario)
-
-    #         # stampo i possibili bersagli dell'effetto
-    #         for indx, bers in enumerate(bersagli, start=1):
-    #             print(f"{indx}- {bers.nome} ({bers.salute}/{bers.salute_max})")
-    #         n_scelte_bersagli = len(bersagli)+1
-    #         print(f"{n_scelte_bersagli}- nessuno")
-    #         indx_bers_scelto =-1 + IU.chiedi_numero(
-    #             "Su chi lo vuoi usare ? ",
-    #             minimo=1,
-    #             massimo= n_scelte_bersagli
-    #         )
-    #         if indx_bers_scelto +1 == n_scelte_bersagli :  # se scelgo nessuno
-    #             bersaglio_scelto = None
-    #         else:
-    #             bersaglio_scelto = bersagli[indx_bers_scelto]
-
-    #         self.usa_oggetto(
-    #             self.oggetti[idx_obj_scelto],
-    #             bersaglio=bersaglio_scelto,
-    #             ambiente=ambiente
-    #         )
-    #     else:
-    #         testo = "L'inventario è vuoto"
-    #         print(testo)
-    #         Log.scrivi_log(testo)
-    #     dati_salvataggio = [self.to_dict(), bersaglio_scelto.to_dict()]
-    #     for dati in dati_salvataggio:
-    #         Json.scrivi_dati("data/salvataggio.json", Json.applica_patch(dati))
-
-    def mostra(self)->str:
+    def mostra_lista_inventario(self)-> list[Oggetto] | str:
         """
-        Stampa il contenuto dell'inventario.
+        metodo che ritorna la lista degli oggetti presenti nell'inventario o una stringa per avvisare che l'inventario è vuoto:
 
         Args:
             None
 
         Return:
-            msg (str): Messaggio che indica il contenuto dell'inventario.
+            list[Oggetto]: lista degli oggetti nell'inventario
+            msg (str): Messaggio per avvisare che l'inventario è vuoto.
+
+        """
+        if len(self.oggetti) == 0:
+            return "L'inventario è vuoto."
+        else:
+            return self.oggetti
+
+    def usa_oggetto(
+        self,
+        oggetto : Oggetto,
+        utilizzatore: Personaggio = None,
+        bersaglio: Personaggio = None,
+        ambiente: Ambiente = None)->str:
+        """
+        Utilizza un oggetto presente nell'inventario.
+
+        Args:
+            oggetto (Oggetto): oggetto da usare.
+            utilizzatore (Personaggio): Il Personaggio che usa l'oggetto se non passato
+            si proverà ad utilizzare quello presente in self.Proprietario se presente.
+            bersaglio(Any): None di Default è un parametro opzionale che
+            permette di usare un oggetto su un altro Personaggio che non sia l'utilizzatore.
+            ambiente (Ambiente): L'ambiente può alterare il funzionamento degli
+            oggetti
+
+        Return:
+            ritorno (str): ritorna una stringa con un messaggio dell'effettivo uso o un messaggio di errore
 
         """
         msg = ""
-        if len(self.oggetti) == 0:
-            msg = "L'inventario è vuoto."
+        if not utilizzatore and self.proprietario:
+            utilizzatore=self.proprietario
+        elif not utilizzatore and not self.proprietario:
+            msg = "manca l'utilizzatore"
+        elif self.cerca_oggetto(oggetto):
+            msg = "l'oggetto non è stato trovato nell'inventario"
         else:
-            msg = "Inventario :\n"
-            for oggetto in self.oggetti :
-                msg +=f"-{oggetto.nome}\n"
+            if not bersaglio:
+                bersaglio = utilizzatore
+            if ambiente is None:
+                mod_ambiente = 0
+            else:
+                mod_ambiente, msg = ambiente.modifica_effetto_oggetto(oggetto)
+                msg += "\n"
+            msg += oggetto.usa(
+                utilizzatore,
+                bersaglio,
+                mod_ambiente=mod_ambiente
+            )
+            self.oggetti.remove(oggetto)
+        #
+        # dati_salvataggio = [self.to_dict(), bersaglio.to_dict()]
+        # for dati in dati_salvataggio:
+        #     Json.scrivi_dati("data/salvataggio.json", Json.applica_patch(dati))
         return msg
 
-    def riversa(self, da_inventario : 'Inventario')->None:
+    def riversa_inventario(self, da_inventario : 'Inventario')-> str:
         """
         Permette ad un inventario di prendere tutti gli oggetti di un altro inventario(da_inventario)
 
@@ -219,25 +162,27 @@ class Inventario(SerializableMixin):
             da_inventario(Inventario): L'inventario da cui vengono prelevati tutti gli oggetti.
 
         Return:
-            None
+            risultato (str): Messaggio che indica gli oggetti trasferiti o che l'inventario è vuoto.
 
         """
+        msg = ""
         if len(da_inventario.oggetti) != 0 :
             if self.proprietario == None:
-                print("Inseriti nell'inventario : ")
-                Log.scrivi_log("Oggetti trasferiti da un inventario a un altro. ")
+                msg = "Inseriti nell'inventario : "
+                # Log.scrivi_log("Oggetti trasferiti da un inventario a un altro. ")
             else:
-                print(f"{self.proprietario.nome} raccoglie :")
-                Log.scrivi_log(f"{self.proprietario.nome} ha raccolto oggetti dall'inventario di un altro personaggio. ")
+                msg = f"{self.proprietario.nome} raccoglie :"
+                # Log.scrivi_log(f"{self.proprietario.nome} ha raccolto oggetti dall'inventario di un altro personaggio. ")
             for oggetto in da_inventario.oggetti :
-                print(f" - {oggetto.nome}")
-                Log.scrivi_log(f"{oggetto.nome} trasferito nell'inventario. ")
-                self.aggiungi(oggetto)
+                msg= f"\n - {oggetto.nome}"
+                # Log.scrivi_log(f"{oggetto.nome} trasferito nell'inventario. ")
+                self._aggiungi(oggetto)
             da_inventario.oggetti.clear()
         else:
             if da_inventario.proprietario == None:
-                testo = "l'inventario è vuoto."
+                msg = "l'inventario è vuoto."
             else:
-                testo = f"L'inventario di {da_inventario.proprietario.nome} è vuoto"
-            print(testo)
-            Log.scrivi_log(testo)
+                msg = f"L'inventario di {da_inventario.proprietario.nome} è vuoto"
+            # Log.scrivi_log(msg)
+        return msg
+
