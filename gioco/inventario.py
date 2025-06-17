9e7091cab5bdbca5d64d9c225eb9bf3af98bebac
KonradC1 <konrad05@gmail.com>
2025-06-17 09:59:19 +0200

    implementazione dei metodi della classe Messaggio

--- gioco/inventario.py
+++ gioco/inventario.py
@@ -1,6 +1,7 @@
 from .oggetto import Oggetto
 from .personaggio import Personaggio
 from .ambiente import Ambiente
+from utils.messaggi import Messaggi
 # from utils.log import Log
 # from utils.salvataggio import SerializableMixin, Json
 # @SerializableMixin.register_class
@@ -14,20 +15,21 @@ class Inventario():
         self.oggetti = []
         self.proprietario = proprietario
 
-    def aggiungi_oggetto(self, oggetto: Oggetto)->str:
+    def aggiungi_oggetto(self, oggetto: Oggetto)->None:
         """
         Aggiungi un oggetto all'inventario.
+        la stringa di testo viene aggiunta alla stringa di messaggi
 
         Args:
             oggetto (Oggetto): L'oggetto da aggiungere all'inventario.
 
         Return:
-            str: risultato dell'aggiunta dell'oggetto all'inventario
+            None
 
         """
         self._aggiungi(oggetto)
         msg = f"Aggiunto l'oggetto '{oggetto.nome}' all inventario. "
-        return msg
+        Messaggi.add_to_messaggi(msg)
 
     def _aggiungi(self, oggetto: Oggetto)-> None:
         """
@@ -45,10 +47,12 @@ class Inventario():
         """
         self.oggetti.append(oggetto)
 
-    def cerca_oggetto(self, oggetto: Oggetto)-> bool | str:
+    def cerca_oggetto(self, oggetto: Oggetto)-> bool | None:
         """
         cerca un oggetto specifico nell'inventario
         ritorna true se è presente o false se non c'è
+        se avviene un errore ritorna None e aggiunge un messaggio di errore
+        alla stringa di messaggi.
 
         Args:
             oggetto (Oggetto): l'elemento da cercare all'interno della lista interna oggetti
@@ -58,7 +62,7 @@ class Inventario():
                 ritorna true se viene trovato
                 ritorna false se non è presente
 
-            msg (str): Messaggio di errore.
+            None.
         """
         msg =""
         try:
@@ -70,17 +74,18 @@ class Inventario():
             return found
         except Exception as e:
             msg = f"Errore generico: {e}"
-            return msg
+            Messaggi.add_to_messaggi(msg)
 
-    def mostra_inventario(self)->str:
+    def mostra_inventario(self)->None:
         """
         invia una stringa con la lista dei nomi degli oggetti presenti
+        alla classe Messaggi.
 
         Args:
             None
 
         Return:
-            msg (str): Messaggio che indica il contenuto dell'inventario.
+            None.
 
         """
         msg = ""
@@ -90,22 +95,25 @@ class Inventario():
             msg = "Inventario :\n"
             for oggetto in self.oggetti :
                 msg +=f"-{oggetto.nome}\n"
-        return msg
+        Messaggi.add_to_messaggi(msg)
 
     def mostra_lista_inventario(self)-> list[Oggetto] | str:
         """
-        metodo che ritorna la lista degli oggetti presenti nell'inventario o una stringa per avvisare che l'inventario è vuoto:
+        metodo che ritorna la lista degli oggetti presenti nell'inventario
+        o invia una stringa a Messaggi per avvisare che l'inventario è vuoto:
 
         Args:
             None
 
         Return:
             list[Oggetto]: lista degli oggetti nell'inventario
-            msg (str): Messaggio per avvisare che l'inventario è vuoto.
+            None: in questo caso viene utilizzarto il metodo statico add_to_messaggi
+            della classe Messaggi per inviare l'invformazione che l'inventario è vuoto.
 
         """
         if len(self.oggetti) == 0:
-            return "L'inventario è vuoto."
+            msg = "L'inventario è vuoto."
+            Messaggi.add_to_messaggi(msg)
         else:
             return self.oggetti
 
@@ -114,7 +122,7 @@ class Inventario():
         oggetto : Oggetto,
         utilizzatore: Personaggio = None,
         bersaglio: Personaggio = None,
-        ambiente: Ambiente = None)->str:
+        ambiente: Ambiente = None)->None:
         """
         Utilizza un oggetto presente nell'inventario.
 
@@ -128,7 +136,7 @@ class Inventario():
             oggetti
 
         Return:
-            ritorno (str): ritorna una stringa con un messaggio dell'effettivo uso o un messaggio di errore
+            None
 
         """
         msg = ""
@@ -156,9 +164,9 @@ class Inventario():
         # dati_salvataggio = [self.to_dict(), bersaglio.to_dict()]
         # for dati in dati_salvataggio:
         #     Json.scrivi_dati("data/salvataggio.json", Json.applica_patch(dati))
-        return msg
+        Messaggi.add_to_messaggi(msg)
 
-    def riversa_inventario(self, da_inventario : 'Inventario')-> str:
+    def riversa_inventario(self, da_inventario : 'Inventario')-> None:
         """
         Permette ad un inventario di prendere tutti gli oggetti di un altro inventario(da_inventario)
 
@@ -166,7 +174,7 @@ class Inventario():
             da_inventario(Inventario): L'inventario da cui vengono prelevati tutti gli oggetti.
 
         Return:
-            risultato (str): Messaggio che indica gli oggetti trasferiti o che l'inventario è vuoto.
+            None
 
         """
         msg = ""
@@ -188,7 +196,7 @@ class Inventario():
             else:
                 msg = f"L'inventario di {da_inventario.proprietario.nome} è vuoto"
             # Log.scrivi_log(msg)
-        return msg
+        Messaggi.add_to_messaggi(msg)
 
     def to_dict(self) -> dict:
         """
