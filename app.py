216e0380fa63cbd333587835f2db252da98dfad9
Matteo0204 <matte.ariotti.02@gmail.com>
2025-06-19 09:56:55 +0200

    Aggiunte importazioni e registrazioni dei blue print battle, view_characters e create_character

--- app.py
+++ app.py
@@ -2,23 +2,33 @@ import os
 from flask import Flask
 from flask_session import Session
 from gioco.routes import gioco
+from battle.routes import battle
+from create_character.routes import create_character
+from view_characters.routes import view_characters
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder='templates',
+        static_folder='static'
+    )
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cambia_questa_chiave_per_una_più_sicura')
+    app.config['SESSION_TYPE'] = 'filesystem'
 
-app = Flask(
-    __name__,
-    template_folder='templates',
-    static_folder='static'
-)
+    app.register_blueprint(gioco)
+    app.register_blueprint(battle)
+    app.register_blueprint(create_character)
+    app.register_blueprint(view_characters)
+    return app
 
 # Imposta una SECRET_KEY sicura (meglio via variabile d'ambiente)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cambia_questa_chiave_per_una_più_sicura')
-app.config['SESSION_TYPE'] = 'filesystem'
 
 # Inizializza il supporto alle sessioni sul filesystem
-Session(app)
+
 
 # Registra il blueprint che contiene tutte le route di gioco
-app.register_blueprint(gioco)
+
 
 if __name__ == '__main__':
     # Modalità di sviluppo con reload automatico
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app = create_app()
+    app.run(debug=True)
