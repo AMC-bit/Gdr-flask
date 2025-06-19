324323631969b5feca15ce99f6fdf50679b2ff72
Matteo0204 <matte.ariotti.02@gmail.com>
2025-06-19 10:11:05 +0200

    Aggiunte importazioni e registrazioni dei blue print inventory e select_environment

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 from flask_session import Session
 from gioco.routes import gioco
-from battle.routes import battle
-from create_character.routes import create_character
-from view_characters.routes import view_characters
+from battle.routes import battle_bp
+from create_character.routes import create_character_bp
+from view_characters.routes import view_characters_bp
+from select_environment.routes import select_environment_bp
+from inventory.routes import inventory_bp
 def create_app():
     app = Flask(
         __name__,
@@ -14,10 +16,13 @@ def create_app():
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'cambia_questa_chiave_per_una_più_sicura')
     app.config['SESSION_TYPE'] = 'filesystem'
 
-    app.register_blueprint(gioco)
-    app.register_blueprint(battle)
-    app.register_blueprint(create_character)
-    app.register_blueprint(view_characters)
+    #app.register_blueprint(gioco)
+    app.register_blueprint(battle_bp)
+    app.register_blueprint(create_character_bp)
+    app.register_blueprint(view_characters_bp)
+    app.register_blueprint(select_environment_bp)
+    app.register_blueprint(inventory_bp)
+
     return app
 
 # Imposta una SECRET_KEY sicura (meglio via variabile d'ambiente)
