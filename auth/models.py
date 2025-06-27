30c509a483f623fe0f3dae66d6bb29dc813617af
delectablerec <ghislaintebou@gmail.com>
2025-06-27 12:08:24 +0200

    Models per la registrazione degli utenti.

--- auth/models.py
+++ auth/models.py
@@ -1,9 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from sqlalchemy.dialects.sqlite import JSON
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=False)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
