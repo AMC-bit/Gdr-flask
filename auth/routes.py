d92e6bdc02613801ee432a93eeb148a18464278c
NicoloPuccini <cordarken90@gmail.com>
2025-06-27 12:40:59 +0200

    sign_in rotta to finish

--- auth/routes.py
+++ auth/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, Flask
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from auth.models import User
 import os
 import re
 
@@ -15,31 +16,32 @@ def psw_proteggi_hash(psw):
 @auth_bp .route('/sign_in')
 def sign_in():
     if request.method == 'POST':
+        name = request.form['name'].strip()
         email = request.form['email'].strip()
         psw = request.form['psw']
         re_psw = request.form['re_psw']
-
-        if not email:
-            raise ValueError('Email cant be empty')
+        if not  name :
+            raise ValueError('Name field cant be empty')
         else:
-            if not controllo_email(email):
-                raise ValueError('Email does not match an email pattern')
+            if not email:
+                raise ValueError('Email cant be empty')
             else:
-                if not ( psw and re_psw and psw == re_psw) :
-                    raise ValueError('Password and repeat Password field must match')
+                if not controllo_email(email):
+                    raise ValueError('Email does not match an email pattern')
                 else:
-                    #Registra il nuovo utente
-                    hash_psw = psw_proteggi_hash(psw)
-                    #TODO qua  hash_psw e email vanno inseriti all'interno del db
-                    """
-                    utente_exist = Utente.query.filter((Utente.email == email)|((Utente.nome == nome )& (Utente.cognome == cognome))).first()
-                    if utente_exist:
-                        messaggio="utente già presente nel db"
+                    if not ( psw and re_psw and psw == re_psw) :
+                        raise ValueError('Password and repeat Password field must match')
                     else:
-                        if nome:
-                            nuovo_utente = Utente(nome= nome, cognome = cognome, email= email, password_hash=generate_password_hash(password))
-                            db.session.add(nuovo_utente)
-                            db.session.commit()
-                    """
-                    return redirect(url_for('login'))
+                        #Registra il nuovo utente
+                        hash_psw = psw_proteggi_hash(psw)
+                        #TODO qua  hash_psw e email vanno inseriti all'interno del db
+                        #Controllo che nel db non ci sia già un utente con quel nome e psw
+                        utente_exist = User.query.filter((User.email == email)|((User.nome == name ))).first()
+                        if utente_exist:
+                            raise ValueError('Utente già presente')
+                        else:
+                                nuovo_utente = User(nome= name, email= email, password_hash= hash_psw, crediti= 100, personaggi=[])
+                                db.session.add(nuovo_utente)
+                                db.session.commit()
+                        return redirect(url_for('login'))
     return render_template('sign_in.html')
