07970dac0ec222aea814b6cb12267db883e0a04e
FabriceGhislain7 <ghislaintebou@gmail.com>
2025-07-04 18:10:51 +0200

    Ho rimpostato i crediti delle funzione che gestiscono i crediti. Ho cancellato i valori usati per i test e ho pulito i commenti unitili.

--- auth/credits.py
+++ auth/credits.py
@@ -14,13 +14,14 @@ def credits_to_create(personaggio: Personaggio) -> int:
     Returns:
         int: crediti necessari alla creazione del personaggio
     """
+
     classe = personaggio.__class__.__name__
     if classe == "Mago":
-        return 2
+        return 20
     elif classe == "Guerriero":
-        return 5
+        return 25
     elif classe == "Ladro":
-        return 7
+        return 50
     else:
         raise ValueError(f"Classe del personaggio non riconosciuta.")
 
@@ -31,7 +32,7 @@ def credits_to_refund(personaggio: Personaggio) -> int:
     Restituisce il numero di crediti rimborsati all'utente in caso di cancellazione
     del personaggio. Il rimborso è un valore fisso in base alla classe del personaggio.
 
-    ("Mago" rimborsa 10 crediti, "Guerriero" 12 crediti, "Ladro" 25 crediti)
+    ("Mago" rimborsa 20 crediti, "Guerriero" 25 crediti, "Ladro" 50 crediti)
 
     Args:
         personaggio (Personaggio): oggetto della classe Personaggio
@@ -39,14 +40,13 @@ def credits_to_refund(personaggio: Personaggio) -> int:
     Returns:
         int: crediti rimborsati all'utente
     """
-    # pg_dict = personaggio.to_dict()
-    # classe = pg_dict['classe']
+
     classe = personaggio.__class__.__name__
     if classe == "Mago":
-        return 2
+        return 20
     elif classe == "Guerriero":
-        return 5
+        return 25
     elif classe == "Ladro":
-        return 7
+        return 50
     else:
         raise ValueError(f"Classe del personaggio non riconosciuta.")
