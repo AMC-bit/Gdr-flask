b24a9378a58ba8492033c5765e5e9dc4c71e438b
Matteo0204 <matte.ariotti.02@gmail.com>
2025-07-15 12:22:47 +0200

    Modificato select_char adattando gli schemas

--- battle/routes.py
+++ battle/routes.py
@@ -4,11 +4,16 @@ from gioco.personaggio import Personaggio
 from gioco.inventario import Inventario
 from gioco.ambiente import Ambiente
 from gioco.missione import Missione
+from characters.routes import load_char, get_owned_chars
+from gioco.schemas.personaggio import PersonaggioSchema
+from gioco.schemas.inventario import InventarioSchema
+from inventory.routes import carica_inventario_da_json
 import random
 import json
 
 classi = {cls.__name__: cls for cls in Personaggio.__subclasses__()}
-
+schema = PersonaggioSchema()
+schema_inv = InventarioSchema()
 @battle_bp.route('/show_inventory', methods=['GET', 'POST'])
 def show_inventory():
     # recupero dalla sessione il personaggio che sta giocando il turno corrente
@@ -73,81 +78,41 @@ def begin_battle():
 def select_char():
     # if request.method == 'POST':
     # prendo i dati da sessione:
-    personaggi = []
-    inventari = []
     missione_corrente = session['missione']
-    if 'personaggi' in session and 'inventari' in session:
-        pg_list = session.get('personaggi', [])
-        inv_list = session.get('inventari',  [])
-        # Deserializzo gli elementi delle liste
-        """
-        for serialized in  pg_list :
-            personaggi.append(Personaggio.from_dict(serialized))
-        for serialized in inv_list:
-            inventari.append(Inventario.from_dict(serialized))
-        #Ora le liste personaggi e  inventari contengono i dati deserializzati
-        """
-    # elementi_selezionati = request.form.getlist('personaggio_id')
-    # if elementi_selezionati:
-    #     for pg in pg_list:
-    #         if pg['id'] in elementi_selezionati:
-    #             personaggi.append(Personaggio.from_dict(pg))
-    #             for inv in inv_list:
-    #                 if pg == inv.proprietario:
-    #                     inventari.append(Inventario.from_dict(inv))
-    #                 break
-
+    pg_id_list = load_char()
+    pg_list = get_owned_chars(pg_id_list)
+    print("TEST",pg_list)
     if request.method == 'POST':
         # Request.form.getlist restituisce una lista di stringhe
         indici_selezionati = request.form.getlist('selected_chars')
         personaggi_selezionati = []
-        inventari_selezionati = []
         # se nel form passi l'indice iallora dobbiamo ricreare il personaggio per conservare i dati in sessione
         for idx in indici_selezionati:
             try:
                 # creazione oggetti
                 for pgs in pg_list:
                     if idx == pgs['id']:
-                        cls = classi.get(pgs.get('classe'))
-                        if cls:
-                            pg = cls.from_dict(pgs)
-                for invs in inv_list:
-                    if invs['id_proprietario'] == idx:
-                        inv = Inventario.from_dict(invs)
-                # aggingiamo i personaggi alle liste
+                        pg = schema.dump(pgs)
                 print("PROVA", type(pg).__name__)
                 personaggi_selezionati.append(pg)
-                inventari_selezionati.append(inv)
             except (ValueError, IndexError):
                 continue
         # serializzo le liste nella sessione
         session['personaggi_selezionati'] = [
-            pg.to_dict() for pg in personaggi_selezionati]
-
-        session['inventari_selezionati'] = [
-            inv.to_dict() for inv in inventari_selezionati]
-
-        # session['missione']= Missione.to_dict(missione_corrente)
+            schema.dump(pg) for pg in personaggi_selezionati]
         # reindirizzo verso la pagina di destinazione
         return redirect(url_for('battle.begin_battle'))
     # eventualmente carico selezione precedente per avere vaolri di default
     selezionati_pg = []
-    selezionati_inv = []
     if 'personaggi_selezionati' in session:
         for sec in session['personaggi_selezionati']:
-            cls = classi.get(sec.get('classe'))
-            if cls:
-                selezionati_pg.append(cls.from_dict(sec))
-        for sec in session.get('inventari_selezionati', []):
-            selezionati_inv.append(Inventario.from_dict(sec))
+            selezionati_pg.append(schema.load(sec))
 
     return render_template(
         'select_char.html',
         personaggi=pg_list,
-        inventari=inv_list,
         missione_corrente=missione_corrente,
-        selezionati_pg=selezionati_pg,
-        selezionati_inv=selezionati_inv)
+        selezionati_pg=selezionati_pg)
 
 
 @battle_bp.route('/test_battle', methods=['GET', 'POST'])
