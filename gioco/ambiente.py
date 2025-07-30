d5bac8f24ce8f89a7cc9ad4f171c8697e2bd6289
KonradC1 <konrad05@gmail.com>
2025-07-30 10:56:35 +0200

    creazione o aggiornamento dei docstring per gli elementi presenti in ambiente.py

--- gioco/ambiente.py
+++ gioco/ambiente.py
@@ -17,16 +17,49 @@ logger = get_logger(__name__)
 class Ambiente:
     """
     Gestisce i modificatori ambientali per Personaggio e Oggetto.
+
+    Attributi:
+        nome (str): Nome dell'ambiente.
+        mod_attacco (int): Modificatore di attacco per i personaggi.
+        mod_cura (int): Modificatore di cura per i personaggi.
+
+    Metodi:
+        modifica_attacco:
+            Modifica l'attacco del personaggio in base alla compatibilità
+            della classe del personaggio con l'ambiente.
+        modifica_effetto_oggetto:
+            Modifica l'effetto di un oggetto in base alla compatibilità
+            dell'oggetto con l'ambiente.
+        modifica_cura:
+            Modifica le capacità di cura del personaggio alla fine di
+            una missione in base alla compatibilità della classe del
+            personaggio con l'ambiente.
     """
     nome: str
     mod_attacco: int = 0
-    mod_cura: float = 0.0
+    mod_cura: int = 0
 
     def modifica_attacco(
         self,
         classe_attaccante: str,
         nome_attaccante: str
     ) -> int:
+        """
+        Modifica l'attacco del personaggio in base alla sinergia
+        tra la classe del personaggio e l'ambiente.
+        Questo metodo deve essere implementato nelle sottoclassi.
+
+        Args:
+            classe_attaccante (str): Classe del personaggio che attacca.
+            nome_attaccante (str): Nome del personaggio che attacca.
+
+        Raises:
+            NotImplementedError:
+                Se il metodo non è implementato nelle sottoclassi.
+
+        Returns:
+            int: Il modificatore di attacco da applicare al personaggio.
+        """
         raise NotImplementedError
 
     def modifica_effetto_oggetto(
@@ -34,23 +67,78 @@ class Ambiente:
         classe_oggetto: str,
         valore_oggetto: int
     ) -> int:
+        """
+        Modifica l'effetto di un oggetto in base alla sua sinergia con
+        l'ambiente circostante.
+        Questo metodo deve essere implementato nelle sottoclassi.
+
+        Args:
+            classe_oggetto (str): Classe dell'oggetto.
+            valore_oggetto (int): Valore dell'effetto dell'oggetto.
+
+        Raises:
+            NotImplementedError:
+                Se il metodo non è implementato nelle sottoclassi.
+
+        Returns:
+            int: Il modificatore di effetto da applicare all'oggetto.
+        """
         raise NotImplementedError
 
     def modifica_cura(self, classe_soggetto: str, nome_soggetto: str) -> int:
+        """
+        Modifica la cura del personaggio alla fine di una missione in base
+        alla capacità della classe del personaggio di
+        curarsi in un ambiente specifico.
+
+        Args:
+            classe_soggetto (str): Classe del soggetto che si cura.
+            nome_soggetto (str): Nome del soggetto che si cura.
+
+        Raises:
+            NotImplementedError: Se il metodo non è implementato nelle sottoclassi.
+
+        Returns:
+            int: Il modificatore di cura da applicare alla cura del soggetto.
+        """
         raise NotImplementedError
+
 
 
 @dataclass
 class Foresta(Ambiente):
+    """
+    Rappresenta un ambiente di foresta con modificatori specifici
+    per attacco e cura.
+    Attributi:
+        nome (str): Nome dell'ambiente, in questo caso "Foresta".
+        mod_attacco (int): Modificatore di attacco per i personaggi
+            che attaccano in questo ambiente.
+        mod_cura (int): Modificatore di cura per i personaggi
+            che si curano in questo ambiente.
+    """
     nome: str = "Foresta"
     mod_attacco: int = 5
-    mod_cura: float = 5.0
+    mod_cura: int = 5
 
     def modifica_attacco(
         self,
         classe_attaccante: str,
         nome_attaccante: str
     ) -> int:
+        """
+        Modifica l'attacco del personaggio in base alla compatibilità
+        della classe del personaggio con l'ambiente Foresta.
+        In questo caso, se il personaggio è un Guerriero,
+        guadagna un bonus di attacco.
+
+        Args:
+            classe_attaccante (str): Classe del personaggio che attacca.
+            nome_attaccante (str): Nome del personaggio che attacca.
+
+        Returns:
+            int: Il modificatore di attacco da applicare al personaggio.
+        """
         if classe_attaccante == "Guerriero":
             logger.info(
                 f"{nome_attaccante} guadagna {self.mod_attacco} "
@@ -64,9 +152,34 @@ class Foresta(Ambiente):
         classe_oggetto: str,
         valore_oggetto: int
     ) -> int:
+        """
+        Modifica l'effetto di un oggetto in base alla sua sinergia con
+        l'ambiente circostante.
+        In questo caso, la foresta non modifica l'effetto degli oggetti.
+
+        Args:
+            classe_oggetto (str): Classe dell'oggetto.
+            valore_oggetto (int): Valore dell'effetto dell'oggetto.
+
+        Returns:
+            int: Il modificatore di effetto da applicare all'oggetto.
+        """
         return 0
 
     def modifica_cura(self, classe_soggetto: str, nome_soggetto: str) -> int:
+        """
+        Modifica la cura del personaggio alla fine di una missione in base
+        alla capacità della classe del personaggio di curarsi in un ambiente.
+        In questo caso, se il personaggio è un Ladro,
+        guadagna un bonus di cura, altrimenti non ci sono bonus o malus.
+
+        Args:
+            classe_soggetto (str): Classe del soggetto che si cura.
+            nome_soggetto (str): Nome del soggetto che si cura.
+
+        Returns:
+            int: Il modificatore di cura da applicare alla cura del soggetto.
+        """
         if classe_soggetto == "Ladro":
             logger.info(
                 f"{nome_soggetto} guadagna {int(self.mod_cura)} cura "
@@ -78,15 +191,41 @@ class Foresta(Ambiente):
 
 @dataclass
 class Vulcano(Ambiente):
+    """
+    Rappresenta un ambiente di vulcano con modificatori specifici
+    per attacco e cura.
+
+    Attributi:
+        nome (str): Nome dell'ambiente, in questo caso "Vulcano".
+        mod_attacco (int): Modificatore di attacco per i personaggi
+            che attaccano in questo ambiente.
+        mod_cura (int): Modificatore di cura per i personaggi
+            che si curano in questo ambiente.
+    """
     nome: str = "Vulcano"
     mod_attacco: int = 10
-    mod_cura: float = -5.0
+    mod_cura: int = -5
 
     def modifica_attacco(
         self,
         classe_attaccante: str,
         nome_attaccante: str
     ) -> int:
+        """
+        Modifica l'attacco del personaggio in base alla compatibilità
+        della classe del personaggio con l'ambiente Vulcano.
+        In questo caso, se il personaggio è un Mago,
+        guadagna un bonus di attacco, mentre se è un Ladro,
+        subisce un malus di attacco.
+
+        Args:
+            classe_attaccante (str): Classe del personaggio che attacca.
+            nome_attaccante (str): Nome del personaggio che attacca.
+
+        Returns:
+            int: Modificatore di attacco da applicare al personaggio.
+        """
+
         if classe_attaccante == "Mago":
             logger.info(
                 f"{nome_attaccante} guadagna {self.mod_attacco} "
@@ -106,6 +245,21 @@ class Vulcano(Ambiente):
         classe_oggetto: str,
         valore_oggetto: int
     ) -> int:
+        """
+        Modifica l'effetto di un oggetto in base alla sua sinergia con
+        l'ambiente circostante.
+        In questo caso, se l'oggetto è una Bomba Acida,
+        guadagna un bonus di danni casuali tra 0 e 15, in caso contrario
+        non ci sono modifiche.
+
+        Args:
+            classe_oggetto (str): Classe dell'oggetto.
+            valore_oggetto (int): Valore dell'effetto dell'oggetto.
+
+        Returns:
+            int: Il modificatore di effetto da applicare all'oggetto.
+        """
+
         if classe_oggetto == "BombaAcida":
             variazione = random.randint(0, 15)
             logger.info(
@@ -116,24 +270,66 @@ class Vulcano(Ambiente):
         return 0
 
     def modifica_cura(self, classe_soggetto: str, nome_soggetto: str) -> int:
+        """
+        Modifica la cura del personaggio alla fine di una missione in base
+        alla capacità della classe del personaggio di curarsi in un ambiente.
+        In questo caso, tutti i personaggi subiscono un malus di cura
+        di 5 punti.
+
+        Args:
+            classe_soggetto (str): Classe del soggetto che si cura.
+            nome_soggetto (str): Nome del soggetto che si cura.
+
+        Returns:
+            int: Il modificatore di cura da applicare alla cura del soggetto.
+        """
         logger.info(
             f"{nome_soggetto} subisce malus di cura {self.mod_cura} "
             f"in Vulcano!"
         )
-        return int(self.mod_cura)
+        return self.mod_cura
 
 
 @dataclass
 class Palude(Ambiente):
+    """
+    Rappresenta un ambiente di palude con modificatori specifici
+    per attacco e cura.
+    Attributi:
+        nome (str): Nome dell'ambiente, in questo caso "Palude".
+        mod_attacco (int): Modificatore di attacco per i personaggi
+            che attaccano in questo ambiente.
+        mod_cura (float): Modificatore di cura per i personaggi
+            che si curano in questo ambiente.
+    Metodi:
+        modifica_attacco: Modifica l'attacco del personaggio in base
+            all'ambiente.
+        modifica_effetto_oggetto: Modifica l'effetto di un oggetto
+            in base all'ambiente.
+        modifica_cura: Modifica la cura del personaggio in base
+            all'ambiente.
+    """
     nome: str = "Palude"
     mod_attacco: int = -5
-    mod_cura: float = 0.3
+    mod_cura: int = 3
 
     def modifica_attacco(
         self,
         classe_attaccante: str,
         nome_attaccante: str
     ) -> int:
+        """
+        Modifica l'attacco del personaggio in base all'ambiente.
+        In questo caso, i Guerrieri e i Ladri subiscono un malus di
+        attacco di 5 punti.
+
+        Args:
+            classe_attaccante (str): Classe del personaggio che attacca.
+            nome_attaccante (str): Nome del personaggio che attacca.
+
+        Returns:
+            int: Il modificatore di attacco da applicare al personaggio.
+        """
         if classe_attaccante in ["Guerriero", "Ladro"]:
             logger.info(
                 f"{nome_attaccante} perde {-self.mod_attacco} "
@@ -147,6 +343,18 @@ class Palude(Ambiente):
         classe_oggetto: str,
         valore_oggetto: int
     ) -> int:
+        """
+        Modifica l'effetto di un oggetto in base all'ambiente.
+        In questo caso, la Pozione Cura subisce una riduzione del
+        suo effetto di cura del 30%.
+
+        Args:
+            classe_oggetto (str): Classe dell'oggetto.
+            valore_oggetto (int): Valore dell'oggetto.
+
+        Returns:
+            int: Il modificatore di effetto da applicare all'oggetto.
+        """
         if classe_oggetto == "PozioneCura":
             riduzione = int(valore_oggetto * self.mod_cura)
             logger.info(
@@ -157,6 +365,26 @@ class Palude(Ambiente):
         return 0
 
     def modifica_cura(self, classe_soggetto: str, nome_soggetto: str) -> int:
+        """
+        Modifica la cura del personaggio in base all'ambiente.
+        In questo caso, i Maghi ricevono un bonus del 12% della loro salute
+        rimanente alla loro capacità di cura.
+
+        Args:
+            classe_soggetto (str): Classe del personaggio che si cura.
+            nome_soggetto (str): Nome del personaggio che si cura.
+
+        Returns:
+            int: Il modificatore di cura da applicare alla cura del personaggio.
+        """
+        if classe_soggetto == "Mago":
+            cura_effettiva = self.mod_cura * 20
+            logger.info(
+                f"{nome_soggetto} incrementa la sua capacità di cura "
+                f"del {(round(float(cura_effettiva/5.0), 2))}% "
+                f"della sua salute rimanente in Palude!"
+            )
+            return cura_effettiva
         return 0
 
 
@@ -166,7 +394,14 @@ class AmbienteFactory:
     """
     @staticmethod
     def get_opzioni() -> Dict[str, Ambiente]:
-        # Permetti selezione sia per numero che per nome utile in console
+        """
+        Metodo statico che restituisce un dizionario di opzioni
+        degli ambienti disponibili.
+        Le chiavi sono sia numeri che nomi degli ambienti,
+        permettendo una selezione flessibile.
+        Returns:
+            Dict[str, Ambiente]: Un dizionario con le opzioni di ambiente.
+        """
         return {
             "1": Foresta(),
             "foresta": Foresta(),
@@ -178,6 +413,18 @@ class AmbienteFactory:
 
     @staticmethod
     def usa_ambiente(scelta: str) -> Ambiente:
+        """
+        Metodo statico che restituisce un ambiente in base alla scelta
+        dell'utente. Se la scelta non è valida, restituisce Foresta
+        come opzione di default
+
+        Args:
+            scelta (str): La scelta dell'utente per l'ambiente.
+
+        Returns:
+            Ambiente: ambiente corrispondente alla scelta dell'utente,
+                o Foresta se la scelta non è valida.
+        """
         mapping = AmbienteFactory.get_opzioni()
         chiave = str(scelta).strip().lower()
         if chiave in mapping:
@@ -191,6 +438,12 @@ class AmbienteFactory:
 
     @staticmethod
     def ambiente_random() -> Ambiente:
+        """
+        Restituisce un ambiente casuale dalla lista degli ambienti disponibili.
+
+        Returns:
+            Ambiente: Un ambiente casuale.
+        """
         opzioni = list(
             {
                 k: v for k, v in AmbienteFactory.get_opzioni().items()
